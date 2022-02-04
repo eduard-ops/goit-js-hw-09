ae862abc9f1cf82812767effd8fbbd395062d131
Edward14887 <epustynnik@gmail.com>
2022-02-04 04:34:55 +0200

    color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const bodyEl = document.querySelector('body');
 
+const buttonStart = document.querySelector('button[data-start]');
+
+const buttonStop = document.querySelector('button[data-stop]');
+
+buttonStart.addEventListener('click', onStartBtnColorClick);
+
+buttonStop.addEventListener('click', onStopBtnColorClick);
+
+let timerId = null;
+
+const NOTIFICATION_DELEY = 1000;
+
+function onStartBtnColorClick() {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, NOTIFICATION_DELEY);
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+}
+
+function onStopBtnColorClick() {
+  clearInterval(timerId);
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
